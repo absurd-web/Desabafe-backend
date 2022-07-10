5eb4983e3874b15bbaad5ad52eee8a8f97854891
absurd-web <2018infor18@gmail.com>
2022-07-10 15:07:19 -0300

    include

--- controllers/records.js
+++ controllers/records.js
@@ -3,12 +3,16 @@ const sequelize = require('../utils/database.js');
 const Message = require('../models/message.js');
 const records = (req, res, next) =>{
     Message.findAndCountAll({
-        attributes: ['Conteudo','Categoria', 'usuarioUsuarioID',[sequelize.fn('count', sequelize.col('Categoria')), 'cnt']],
+        attributes: {
+            include: [
+                [sequelize.fn('count', sequelize.col('Categoria')), 'cnt']
+            ]
+        },
         group: "Categoria",
     })
     .then((db)=>{
         if(db.count != 0){
-            return res.status(200).json(db);
+            return res.status(200).json({db});
         }else{
             return res.status(404).json({message: "Nenhum registro encontrado"});
         }
